096606b35d38726d49eff86ea4e59c4084690c20
seahi007 <seahi@foxmail.com>
2023-04-22 00:25:02 +0800

    frist commit

---
+++ components/inc/device.h
@@ -0,0 +1,16 @@
+/**
+ * @file device.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-22
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#ifndef DEVICE_H
+#define DECVICE_H
+
+
+
+#endif
---
+++ components/src/device.c
@@ -0,0 +1,18 @@
+/**
+ * @file device.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-04-22
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include "blog.h"
+#include "bl_gpio.h"
+#include "device.h"
